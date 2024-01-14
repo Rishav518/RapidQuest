3178adce4903b65b58dca5dcf7e80cf92d8497c0
Dristi Bhugun <db1178@live.mdx.ac.uk>
2024-01-14 14:43:23 +0530

    final

--- js/script.js
+++ js/script.js
@@ -14,3 +14,21 @@ window.onload = function() {
         retoutput.innerHTML = this.value ;
     }
 }
+
+//in mobile view, the navbar will collapse when user scroll down
+
+let lastScrollTop = 0;
+
+window.addEventListener("scroll", function() {
+  const currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (currentScroll > lastScrollTop) {
+    // Scrolling down
+    document.querySelector('.navbar').classList.add('hidden');
+  } else {
+    // Scrolling up
+    document.querySelector('.navbar').classList.remove('hidden');
+  }
+
+  lastScrollTop = currentScroll <= 0 ? 0 : currentScroll; // For Mobile or negative scrolling
+}, false);
